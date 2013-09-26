665ca54f45bf6a909018eed9630ae9c82798a2d8
zpasal <zaharije@atlantbh.com>
2013-09-26 12:08:44 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Emplight::Application.config.secret_token = '77c402dd3581987c9753c3126d7436328259ab2d324ac37bb771cd411f9a1f29921f47e2bb0fa72c95cb921ed5e6655be9a0f206be3851fb418977268ef7b3d0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Emplight::Application.config.session_store :cookie_store, key: '_emplight_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Emplight::Application.config.session_store :active_record_store
